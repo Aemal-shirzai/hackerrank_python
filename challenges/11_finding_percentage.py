4fbcb11db90d92f154d715ed5e8de53400cd51b9
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-01-28 17:13:59 -0500

    [IMP] Solve challenge 11.

---
+++ challenges/11_finding_percentage.py
@@ -0,0 +1,18 @@
+"""
+The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. 
+Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+    line = student_marks.get(query_name, [])
+    average = sum(line) / len(line)
+    print("%.2f" % average)
