dcb132df0dd25e2ca6e930f5d357f90ebdb52b1d
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-02-03 13:49:13 -0500

    [IMP] Solve capitalize challenge 24.

---
+++ challenges/24_capitalize.py
@@ -0,0 +1,19 @@
+import math
+import os
+import random
+import re
+import sys
+
+def solve(s):
+    return " ".join(word.capitalize()  for word in s.split(' '))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
