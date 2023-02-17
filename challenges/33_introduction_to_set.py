b1cdbc8cd8eedd31af0b90ceef59482fdbd8cde3
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-02-16 21:44:39 -0500

    [IMP] Solve Challenge sets 33.

---
+++ challenges/33_introduction_to_set.py
@@ -0,0 +1,9 @@
+def average(array):
+    array = set(array)
+    return sum(array) / len(array)
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
