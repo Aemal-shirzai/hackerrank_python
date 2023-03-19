b9694b0927e65a8d40232deddea8087c2e11199a
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-03-18 22:16:21 -0400

    [IMP] Solve map and lambda function challenge 70

---
+++ challenges/70_map_and_lambda_function.py
@@ -0,0 +1,14 @@
+cube = lambda x: pow(x, 3)
+
+def fibonacci(N):
+    fab_lis = [0,1]
+
+    for i in range(2, N):
+        fab_lis.append(fab_lis[i-2] + fab_lis[i-1])
+    
+    print(fab_lis[0:N])
+    return(fab_lis[0:N])
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
