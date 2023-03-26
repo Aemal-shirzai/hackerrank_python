01cacb568e3151510551eee05bb5bdf76ac75d32
aemal-shirzai <aemalshirzai2016@gmail.com>
2023-03-26 16:30:05 -0400

    [FIX] Fix issue in challenge 71.

--- challenges/71_dealing_with_complex_numbers.py
+++ challenges/71_dealing_with_complex_numbers.py
@@ -44,3 +44,8 @@ class Complex(object):
         return result
 
 if __name__ == '__main__':
+    c = map(float, input().split())
+    d = map(float, input().split())
+    x = Complex(*c)
+    y = Complex(*d)
+    print(*map(str, [x+y, x-y, x*y, x/y, x.mod(), y.mod()]), sep='\n')
